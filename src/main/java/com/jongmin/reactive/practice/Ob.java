29a1052af9968e5bb0b64af04f88b9c69c3484c6
jm-kim <jm-kim@linecorp.com>
2019-10-14 00:39:12 +0900

    Iterator

--- src/main/java/com/jongmin/reactive/practice/Ob.java
+++ src/main/java/com/jongmin/reactive/practice/Ob.java
@@ -1,6 +1,6 @@
 package com.jongmin.reactive.practice;
 
-import java.util.Arrays;
+import java.util.Iterator;
 
 /**
  * 1. Reactive 관련된 아이디어를 이야기 할 때 Duality(쌍대성)라는 용어가 등장한다.
@@ -9,9 +9,26 @@ import java.util.Arrays;
  */
 public class Ob {
     public static void main(String[] args) {
-        // List > Collection > Iterable
-        // Iterable interface를 구현하면 "for-each Loop" 구문의 target이 될 수 있다.
-        Iterable<Integer> iter = Arrays.asList(1, 2, 3, 4, 5);
+        Iterable<Integer> iter = new Iterable<Integer>() {
+            @Override
+            public Iterator<Integer> iterator() {
+                return new Iterator<Integer>() {
+                    int i = 0;
+                    final static int MAX = 10;
+
+                    @Override
+                    public boolean hasNext() {
+                        return i < MAX;
+                    }
+
+                    @Override
+                    public Integer next() {
+                        return ++i;
+                    }
+                };
+            }
+        };
+
         for (Integer i : iter) {
             System.out.println(i);
         }
