1929552dacf1c14f5146a54b7207f83bfb2ba9e7
jm-kim <jm-kim@linecorp.com>
2019-10-15 00:11:29 +0900

    Observer

--- src/main/java/com/jongmin/reactive/practice/Ob.java
+++ src/main/java/com/jongmin/reactive/practice/Ob.java
@@ -1,36 +1,43 @@
 package com.jongmin.reactive.practice;
 
-import java.util.Iterator;
+import java.util.Observable;
+import java.util.Observer;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 1. Reactive 관련된 아이디어를 이야기 할 때 Duality(쌍대성)라는 용어가 등장한다.
  * 2. Observer Pattern
  * 3. Reactive Streams - 표준 -> Java9 JDK에 추가됨
  */
+@Slf4j
 public class Ob {
+    // Source -> Event/Data -> Observer
+    static class IntObservable extends Observable implements Runnable {
+
+        @Override
+        public void run() {
+            for (int i = 1; i <= 10; i++) {
+                setChanged();
+                notifyObservers(i);         // push
+                // int i = it.next()        // pull
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        Iterable<Integer> iter = new Iterable<Integer>() {
+        // Iterable <---> Observable (duality)
+        // Pull     <---> Push
+        Observer ob = new Observer() {
             @Override
-            public Iterator<Integer> iterator() {
-                return new Iterator<Integer>() {
-                    int i = 0;
-                    final static int MAX = 10;
-
-                    @Override
-                    public boolean hasNext() {
-                        return i < MAX;
-                    }
-
-                    @Override
-                    public Integer next() {
-                        return ++i;
-                    }
-                };
+            public void update(Observable o, Object arg) {
+                log.info("{}", arg);
             }
         };
 
-        for (Integer i : iter) {
-            System.out.println(i);
-        }
+        IntObservable io = new IntObservable();
+        io.addObserver(ob);
+
+        io.run();
     }
 }
