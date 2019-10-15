4156928a4e43c82439ebcb7c12369a6b4972d083
jm-kim <jm-kim@linecorp.com>
2019-10-15 23:37:14 +0900

    mapPub

--- src/main/java/com/jongmin/reactive/practice/PubSub2.java
+++ src/main/java/com/jongmin/reactive/practice/PubSub2.java
@@ -1,6 +1,7 @@
 package com.jongmin.reactive.practice;
 
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -10,13 +11,48 @@ import org.reactivestreams.Subscription;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * Publisher -> [Data1] -> Operator1 -> [Data2]-> Operator2 -> [Data3] -> Subscriber
+ * 1. map (d1 -> f -> d2)
+ *      pub -> [Data1] -> mapPub -> [Data2]  -> logSub
+ */
 @Slf4j
 public class PubSub2 {
     public static void main(String[] args) {
         Publisher<Integer> pub = iterPub(Stream.iterate(1, a -> a + 1)
                                                .limit(10)
                                                .collect(Collectors.toList()));
-        pub.subscribe(logSub());
+        Publisher<Integer> mapPub = mapPub(pub, s -> s * 10);
+        mapPub.subscribe(logSub());
+    }
+
+    private static Publisher<Integer> mapPub(Publisher<Integer> pub, Function<Integer, Integer> f) {
+        return new Publisher<Integer>() {
+            @Override
+            public void subscribe(Subscriber<? super Integer> sub) {
+                pub.subscribe(new Subscriber<Integer>() {
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        sub.onSubscribe(s);
+                    }
+
+                    @Override
+                    public void onNext(Integer i) {
+                        sub.onNext(f.apply(i));
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        sub.onError(t);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        sub.onComplete();
+                    }
+                });
+            }
+        };
     }
 
     private static Subscriber<Integer> logSub() {
