a5ddb93775cf4f1efd2c5b413d9ced07da06abca
jm-kim <jm-kim@linecorp.com>
2019-10-17 23:48:31 +0900

    SchedulerEx

---
+++ src/main/java/com/jongmin/reactive/practice/SchedulerEx.java
@@ -0,0 +1,59 @@
+package com.jongmin.reactive.practice;
+
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SchedulerEx {
+    public static void main(String[] args) {
+        Publisher<Integer> pub = new Publisher<Integer>() {
+            @Override
+            public void subscribe(Subscriber<? super Integer> sub) {
+                sub.onSubscribe(new Subscription() {
+                    @Override
+                    public void request(long n) {
+                        sub.onNext(1);
+                        sub.onNext(2);
+                        sub.onNext(3);
+                        sub.onNext(4);
+                        sub.onNext(5);
+                        sub.onComplete();
+                    }
+
+                    @Override
+                    public void cancel() {
+
+                    }
+                });
+            }
+        };
+
+        Subscriber<Integer> sub = new Subscriber<Integer>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+                log.info("onSubscribe");
+                s.request(Long.MAX_VALUE);
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                log.info("onNext: {}", integer);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                log.info("onError", t);
+            }
+
+            @Override
+            public void onComplete() {
+                log.info("onComplete");
+            }
+        };
+
+        pub.subscribe(sub);
+    }
+}
