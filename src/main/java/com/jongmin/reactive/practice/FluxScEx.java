60c05eafd13ce8965050dafd1bcf14b7daa67450
jm-kim <jm-kim@linecorp.com>
2019-10-18 00:32:45 +0900

    FluxScEx

---
+++ src/main/java/com/jongmin/reactive/practice/FluxScEx.java
@@ -0,0 +1,18 @@
+package com.jongmin.reactive.practice;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+@Slf4j
+public class FluxScEx {
+    public static void main(String[] args) {
+        Flux.range(1, 10)
+            .publishOn(Schedulers.newSingle("pub"))
+            .log()
+            .subscribeOn(Schedulers.newSingle("sub"))
+            .subscribe(System.out::println);
+
+        log.info("exit");
+    }
+}
